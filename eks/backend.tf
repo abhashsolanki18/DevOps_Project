bead015873e463d36da3c9719810114e9fed302f
Abhash Solanki <157798417+abhashsolanki18@users.noreply.github.com>
2025-05-18 12:28:32 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "as-tf-bucket1"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
